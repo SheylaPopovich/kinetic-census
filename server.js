808f7ed9e0d1b2b20f7a88874e8090fe84ce883f
Sheyla Popovich <sheylapopovich@gmail.com>
2021-10-24 11:58:12 -0600

    linked JS files to HTML index

--- server.js
+++ server.js
@@ -17,19 +17,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 
-// const db = require("../kinetic-census/Develop/models");
-
-// require("./Develop/routes/apiRoutes")(app);
-// require("./Develop/routes/htmlRoutes")(app);
-
-
-
-
-// app.listen(PORT, () => {
-//   console.log(`App running on port ${PORT}!`);
-// });
-
-
 const db = require("../kinetic-census/Develop/models");
 
 require("./Develop/routes/apiRoutes")(app);
