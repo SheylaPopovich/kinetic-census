215fd237a3366be96cc89371eff160db2e7b9956
Sheyla Popovich <sheylapopovich@gmail.com>
2021-10-29 09:22:19 -0600

    formatted server.js file

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-// const db = require("./models");
 const logger = require("morgan");
-
 
 const PORT = process.env.PORT || 3000;
 
@@ -14,20 +12,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: true,
   useCreateIndex: true,
-  useUnifiedTopology: false
+  useUnifiedTopology: false,
 });
-
 
 app.use(require("./routes/apiRoutes.js"));
 app.use(require("./routes/htmlRoutes.js"));
-
-
-// require("./Develop/routes/apiRoutes")(app);
-// require("./Develop/routes/htmlRoutes")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
