7f7d487dd5e2d76690f873119ec50be792a41fd2
Sheyla Popovich <sheylapopovich@gmail.com>
2021-10-30 13:19:45 -0600

    deploy to Heroku

--- models/workout.js
+++ models/workout.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout");
+
 const workoutSchema = new Schema(
   {
     day: {
